fc6c0f4744609e8349180624f17fec311a4f78ab
Aseel Basheer <asbasheer@my.okcu.edu>
2015-03-25 15:37:59 -0500

    This is the second time of using git

---
+++ factCalculation.c
@@ -0,0 +1,7 @@
+#include(stdio.h>
+int factorial(int n);
+
+int main(){
+printf("The factorial of 5 is %d\n",factorial(5));
+return(0);
+}
---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int s;
+int x;
+int y;
+printf("Please enter the first number\n");
+scanf(x,%d);
+printf("Please enter the second number\n");
+scanf(y);
+s=x+y;
+printf("The sum of %d and %d is : %d ",x,y,s);
+return(0);
+}
